810008f4b0ab5bcf9f4a037ba07126d96a322611
PavanGhadage <pavanghadage510@gmail.com>
2026-05-26 16:10:37 +0530

    nes

--- NodeJS/express_app/day_6/index.js
+++ NodeJS/express_app/day_6/index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+const home = {
+  head: "Welcome to My Portfolio",
+  para: "I am a Web Developer passionate about creating websites.",
+};
+
+const about = {
+  head: "About me",
+  para: "I am a Frontend Developer skilled in HTML, CSS, JavaScript, React, and Node.js.",
+};
+const services = {
+  one: "",
+};
+
 app.get("/", (req, res) => {
-  res.render("Home.ejs");
+  res.render("Home", { data: home });
 });
 app.get("/contact", (req, res) => {
-  res.render("Contact.ejs");
+  res.render("Contact.ejs", { data: about });
 });
 app.get("/About", (req, res) => {
-  res.render("About.ejs");
+  res.render("About.ejs", { data: about });
 });
 app.get("/services", (req, res) => {
   res.render("Services.ejs");
