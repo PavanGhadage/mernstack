17bb3bcf0aa8ae20c6191e0d4034dd2ced0ea204
PavanGhadage <pavanghadage510@gmail.com>
2026-07-16 17:04:27 +0530

    new

--- React/Project/food_order_app/backend/server.js
+++ React/Project/food_order_app/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
 const dns = require("dns");
+
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 require("dotenv").config();
@@ -13,25 +14,37 @@ const restaurantRoutes = require("./routes/restaurantRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const contactRoutes = require("./routes/contactRoutes");
-const cloudinary = require("./config/cloudinary");
 
 const app = express();
 
-// CONNECT MONGODB
+// CONNECT DATABASE
 connectDB();
 
-app.use(cors());
+// MIDDLEWARE
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
+// ROUTES
 app.use("/api/users", userRoutes);
 app.use("/api/restaurants", restaurantRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/contact", contactRoutes);
 
+// TEST ROUTE
+app.get("/", (req, res) => {
+  res.send("FoodieHub Backend is Running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running as running ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
